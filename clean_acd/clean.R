4d4a4cb927071ac99e8269180651814a473cb31a
Fran Villamil <franvillamil@gmail.com>
2023-02-02 20:39:14 +0100

    i cleaned the ACD

--- clean_acd/clean.R
+++ clean_acd/clean.R
@@ -1,3 +1,11 @@
 setwd("~/Desktop/my_R_project")
+library(dplyr)
 
 data = read.csv("input_data/ucdp-prio-acd-221.csv")
+
+conflict_years = data %>%
+  group_by(side_a, gwno_a) %>%
+  summarize(conflict_years = n())
+
+write.csv(conflict_years,
+  "clean_acd/output/clean_data.csv")
