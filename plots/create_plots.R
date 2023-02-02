c843ae81571e53d58b53f5c594833949a70b05a4
Fran Villamil <franvillamil@gmail.com>
2023-02-02 20:46:48 +0100

    ..

--- plots/create_plots.R
+++ plots/create_plots.R
@@ -5,5 +5,5 @@ data = read.csv("clean_acd/output/clean_data.csv")
 
 pdf("plots/output/prueba.pdf")
 ggplot(data, aes(x = conflict_years)) +
-  geom_histogram()
+  geom_histogram() + theme_bw()
 dev.off()
